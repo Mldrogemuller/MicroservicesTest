98e49c7150908420c6fb13dbf9eef98338f387f2
magnusdrogemuller <mldrogemuller@gmail.com>
2024-02-29 16:52:55 +0100

    Made Project Containerized with Docker, Documentation in Makefile

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from mylib.logic import search_wiki
 from mylib.logic import wiki as wikilogic
+from mylib.logic import phrase as wikiphrases
 from fastapi import FastAPI
 import uvicorn
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Wikipedia API. Call /search or /wiki"}
+
 
 @app.get("/search/{value}")
 async def search(value: str):
@@ -16,6 +19,7 @@ async def search(value: str):
     result = search_wiki(value)
     return {"result": result}
 
+
 @app.get("/wiki/{name}")
 async def wiki(name: str):
     """Page to search Wikipedia name"""
@@ -23,7 +27,13 @@ async def wiki(name: str):
     result = wikilogic(name)
     return {"result": result}
 
+
+@app.get("/phrase/{name}")
+async def phrase(name: str):
+    """Page to search Wikipedia name"""
+    result = wikiphrases(name)
+    return {"result": result}
+
+
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
-
-
