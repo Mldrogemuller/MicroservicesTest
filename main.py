a6839840b7ee6c8c2fee2c0ffcb4ee757aa9c35a
magnusdrogemuller <mldrogemuller@gmail.com>
2024-02-29 16:21:32 +0100

    FastAPI connection with two methods to take search_wiki, and wiki

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-from mylib import logic
+from mylib.logic import search_wiki
+from mylib.logic import wiki as wikilogic
+from fastapi import FastAPI
+import uvicorn
 
-print(logic.wiki())
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API. Call /search or /wiki"}
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """Page to search in wikipedia"""
+
+    result = search_wiki(value)
+    return {"result": result}
+
+@app.get("/wiki/{name}")
+async def wiki(name: str):
+    """Page to search Wikipedia name"""
+
+    result = wikilogic(name)
+    return {"result": result}
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
 
 
